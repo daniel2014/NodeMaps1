a859362c2be744e87892a6210b8a1c79f8377cf4
Daniel <danielrsoares2006@gmail.com>
2018-05-24 22:50:04 -0300

    24.05.2018 v:0001 Criado Layouts

---
+++ app/src/main/java/com/danielrsoares/nodemaps/activity/InventarioActivity.java
@@ -0,0 +1,15 @@
+package com.danielrsoares.nodemaps.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.danielrsoares.nodemaps.R;
+
+public class InventarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventario);
+    }
+}
---
+++ app/src/main/java/com/danielrsoares/nodemaps/activity/LoginUsuarioActivity.java
@@ -0,0 +1,15 @@
+package com.danielrsoares.nodemaps.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.danielrsoares.nodemaps.R;
+
+public class LoginUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_usuario);
+    }
+}
